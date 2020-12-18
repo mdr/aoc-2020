2ce8745a579cd90567fb9615652c9b7dcaeef20a
Matt Russell <matthew.russell@sonocent.com>
2020-12-18 08:58:46 +0000

    rm AST

--- src/main/scala/aoc/day18/Day18.scala
+++ src/main/scala/aoc/day18/Day18.scala
@@ -5,43 +5,29 @@ import aoc.util.Util
 import scala.util.chaining.scalaUtilChainingOps
 import scala.util.parsing.combinator.RegexParsers
 
-sealed trait Expression {
-  def eval: Long = this match {
-    case Number(n) => n
-    case AddExpr(left, right) => left.eval + right.eval
-    case MultExpr(left, right) => left.eval * right.eval
-  }
-}
-
-case class Number(n: Long) extends Expression
-
-case class AddExpr(left: Expression, right: Expression) extends Expression
-
-case class MultExpr(left: Expression, right: Expression) extends Expression
-
 trait ExpressionParsers extends RegexParsers {
 
-  val expression: Parser[Expression]
+  val expression: Parser[Long]
 
-  lazy val number: Parser[Number] = """(0|[1-9]\d*)""".r ^^ (s => Number(s.toLong))
+  lazy val number: Parser[Long] = """(0|[1-9]\d*)""".r ^^ (_.toLong)
 
-  def apply(s: String): Expression =
+  def apply(s: String): Long =
     parse(expression, s) match {
       case Success(expr, _) => expr
       case Failure(msg, _) => throw new RuntimeException(msg)
       case Error(msg, _) => throw new RuntimeException(msg)
     }
 
-  lazy val expr0: Parser[Expression] = number | ("(" ~> expression <~ ")")
+  lazy val expr0: Parser[Long] = number | ("(" ~> expression <~ ")")
 
 }
 
 object ExpressionParser extends ExpressionParsers {
 
-  lazy val expression: Parser[Expression] = expr0 ~ rep(("+" | "*") ~ expr0) ^^ { case lhs ~ rhss =>
+  lazy val expression: Parser[Long] = expr0 ~ rep(("+" | "*") ~ expr0) ^^ { case lhs ~ rhss =>
     rhss.foldLeft(lhs) {
-      case (x, "+" ~ y) => AddExpr(x, y)
-      case (x, "*" ~ y) => MultExpr(x, y)
+      case (x, "+" ~ y) => x + y
+      case (x, "*" ~ y) => x * y
     }
   }
 
@@ -49,12 +35,12 @@ object ExpressionParser extends ExpressionParsers {
 
 object ExpressionParser2 extends ExpressionParsers {
 
-  lazy val expression: Parser[Expression] = addExpr ~ rep("*" ~ addExpr) ^^ { case lhs ~ rhss =>
-    rhss.foldLeft(lhs) { case (x, "*" ~ y) => MultExpr(x, y) }
+  lazy val expression: Parser[Long] = addExpr ~ rep("*" ~ addExpr) ^^ { case lhs ~ rhss =>
+    rhss.foldLeft(lhs) { case (x, "*" ~ y) => x * y }
   }
 
-  lazy val addExpr: Parser[Expression] = expr0 ~ rep("+" ~ expr0) ^^ { case lhs ~ rhss =>
-    rhss.foldLeft(lhs) { case (x, "+" ~ y) => AddExpr(x, y) }
+  lazy val addExpr: Parser[Long] = expr0 ~ rep("+" ~ expr0) ^^ { case lhs ~ rhss =>
+    rhss.foldLeft(lhs) { case (x, "+" ~ y) => x + y }
   }
 
 }
@@ -65,8 +51,8 @@ object Day18 extends App {
 
   def solvePartTwo(inputPath: String): Unit = solve(inputPath, ExpressionParser2.apply)
 
-  private def solve(inputPath: String, parser: String => Expression): Unit =
-    Util.loadLines(inputPath).map(parser).map(_.eval).sum.tap(println)
+  private def solve(inputPath: String, parser: String => Long): Unit =
+    Util.loadLines(inputPath).map(parser).sum.tap(println)
 
   println("Part One")
   solvePartOne("day18/example.txt")
